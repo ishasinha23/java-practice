daf799df6332cc9c411af5ac090dd1cf1b27edc3
Isha Sinha <ishasinha8511@gmail.com>
2026-06-05 17:48:24 +0530

    Create Arthematic.java

---
+++ Day2/Arthematic.java
@@ -0,0 +1,17 @@
+public class Arthematic {
+  public static void main (String[] args)
+  {
+    int a=10;
+    int b=40;
+    int sum=a+b;
+    System.out.println(sum);
+    int diff=b-a;
+    System.out.println(diff);
+    int mul=a*b;
+    System.out.println(mul);
+    int div=a/b;
+    System.out.println(div);
+    int module=a%b;
+    System.out.println(module);
+  }
+}
