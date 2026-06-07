1f9901b3a11b438983247a1c92186e29f3e87651
Isha Sinha <ishasinha8511@gmail.com>
2026-06-07 23:12:39 +0530

    Create Loop.java

---
+++ Day4/Loop.java
@@ -0,0 +1,7 @@
+public class loop {
+    public static void main (String[] args) {
+        for (int counter=0; counter<=3; counter++) {
+            System.out.println("Hello World");
+        }
+    }
+}
